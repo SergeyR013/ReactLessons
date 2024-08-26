c59dfa049a67489dddf0e7e15c967f413bcd9eca
SergeyR013 <56975358+SergeyR013@users.noreply.github.com>
2024-08-26 15:08:25 +0300

    Сделал прогрессбар

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -1,9 +1,11 @@
 import { Footer } from "../footer/footer";
 import { Header } from "../header/header";
+import { ProgressBar } from "../progressbar/progressbar.jsx"
 
 export const Layout = ({ children }) => {
     return (
         <div>
+            <ProgressBar/>
             <Header />
             {children}
             <Footer />
